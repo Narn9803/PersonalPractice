6bdd7764067e57c653db13a8394c5176997216e8
gitpract1 <aravindhnm9803@gmail.com>
2026-05-25 22:22:20 +0530

    Changes done in files

--- jspract10.js
+++ jspract10.js
@@ -1,22 +1,23 @@
-// let flavours = ["Vanila", "ButterScoch", "Strawberry", "Mango"];
-// console.log("flavours "+ flavours[flavours.length-2]);
-// let sampleArr = ["Choc", true, null, 1212, {id:2}];
-// let vval = sampleArr[sampleArr.length-1].id; 
-// console.log(sampleArr[sampleArr.length-1], vval);
-//Array Constructor
+let flavours = ["Vanila", "ButterScoch", "Strawberry", "Mango"];
+console.log("flavours "+ flavours[flavours.length-2]);
 
+let sampleArr = ["Choc", true, null, 1212, {id:2}];
+console.log("sampleArr", sampleArr[sampleArr.length-1]);
+
+let vval = sampleArr[sampleArr.length-1].id; 
+console.log("vval", vval);
+
+//Array Constructor
 // let arr = new Array();
 // arr[0] = "Good";
 // arr[1] = "Bad";
 // arr[2] = "Ugly";
-
 // console.log("array", arr[arr.length-3]);
 
 //Dense Array
-
 let a1 = [1,2,3,4,5]; //contigous memory
 console.log("a1", a1);
 
-// // Sparse array
+//Sparse array
 let a2 = [3,,2,,5,6]; //hash table or hash map
 console.log("a2", a2);
